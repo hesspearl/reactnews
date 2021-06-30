79b0a22aa40af8c660596b4843f3f7f823fc8d20
Hesaa <hesspearldev@gmail.com>
2021-06-30 19:35:22 +0300

    header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable @next/next/no-img-element */
+export function Header () {
+    return(
+    <header>
+    <div>
+        <img src="/images/logo.svg" alt=""/>
+    </div>
+    <nav>
+        <a>Home</a>
+        <a>Posts</a>
+    </nav>
+     </header>
+    )
+}
